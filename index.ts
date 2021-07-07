2403f6ba7131079bef8816bb0e74015e7c4e03cf
Miloš Spasojević <milosspasojevic@Miloss-MacBook-Pro.local>
2021-07-07 14:00:19 +0200

    update response

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ addEventListener("fetch", (event) => {
     // Get the value of the header named `x-forwarded-for`.
     // The value can either be a IPv4 or IPv6 address.
     const ip = event.request.headers.get("x-forwarded-for");
-    const headers = JSON.stringify(event.request.headers);
-    const response = new Response(`Your IP address is <b>${ip}</b><br />Headers: ${headers}`, {
+    const customHeader = event.request.headers.get("x-test2");
+    const response = new Response(`Your IP address is <b>${ip}</b><br />Header: ${customHeader}`, {
       headers: { "content-type": "text/html" },
     });
     event.respondWith(response);
