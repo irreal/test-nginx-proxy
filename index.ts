6bbfe83e65a506c5d1ccea131de0b09aba118f55
Miloš Spasojević <milosspasojevic@Miloss-MacBook-Pro.local>
2021-07-07 11:51:13 +0200

    print back received ip address

---
+++ index.ts
@@ -0,0 +1,11 @@
+/// <reference types="./deployctl.d.ts" />
+
+addEventListener("fetch", (event) => {
+    // Get the value of the header named `x-forwarded-for`.
+    // The value can either be a IPv4 or IPv6 address.
+    const ip = event.request.headers.get("x-forwarded-for");
+    const response = new Response(`Your IP address is <b>${ip}</b>`, {
+      headers: { "content-type": "text/html" },
+    });
+    event.respondWith(response);
+  });
