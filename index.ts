17a7b4ac99f72c341db59ce254f40ac82c67ea29
Miloš Spasojević <milosspasojevic@Miloss-MacBook-Pro.local>
2021-07-08 10:12:01 +0200

    show path

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
-/// <reference types="./deployctl.d.ts" />
+import {} from "./deployctl.d.ts";
 
 addEventListener("fetch", (event) => {
-    // Get the value of the header named `x-forwarded-for`.
-    // The value can either be a IPv4 or IPv6 address.
-    const ip = event.request.headers.get("x-forwarded-for");
-    const customHeader = event.request.headers.get("x-test2");
-    const response = new Response(`Your IP address is <b>${ip}</b><br />Header: ${customHeader}`, {
+  // Get the value of the header named `x-forwarded-for`.
+  // The value can either be a IPv4 or IPv6 address.
+  const ip = event.request.headers.get("x-forwarded-for");
+  const customHeader = event.request.headers.get("x-test2");
+  const path = event.request.url;
+  const response = new Response(
+    `Your IP address is <b>${ip}</b><br />Header: ${customHeader} <br />Url: ${path}`,
+    {
       headers: { "content-type": "text/html" },
-    });
-    event.respondWith(response);
-  });
+    }
+  );
+  event.respondWith(response);
+});
